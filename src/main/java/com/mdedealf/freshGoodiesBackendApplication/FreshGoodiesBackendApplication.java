9b0fa4ac1d141a191a5f393ec8e972821ff89f1e
mdedealf <dede.alfaruqq@gmail.com>
2024-10-25 11:11:36 +0700

    initial commit

---
+++ src/main/java/com/mdedealf/freshGoodiesBackendApplication/FreshGoodiesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mdedealf.freshGoodiesBackendApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreshGoodiesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreshGoodiesBackendApplication.class, args);
+	}
+
+}
